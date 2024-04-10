b8da1e253dcca0814602f0566ec95db03c3949ae
Eder Borges <eder1fbds@gmail.com>
2024-04-10 18:02:58 -0300

    Teste_Estrutura

--- src/App.js
+++ src/App.js
@@ -2,9 +2,48 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const LavaBar = [
+    {
+      cliente: "Vinicius",
+      carro: "Mitsubishi Lancer",
+      bairro: "Santa Luzia",
+      mensalista: true,
+      mensal_pago: true,
+      Idade: 25,
+      bebida: {
+        quente:'whisky White-Horse',
+        cerveja:'Skol'
+      }
+    },
+    {
+      cliente: "Eder",
+      carro: "Chevrolet Corsa",
+      bairro: "Santa Luzia",
+      mensalista: true,
+      mensal_pago: false,
+      Idade: 23,
+      bebida: {
+        quente: "Whisky Ballantine's",  
+        cerveja: "Cerveja Original"
+      }
+    },
+    {
+      cliente: "Debora",
+      carro: "Volkswagen Gol",
+      bairro: "Ouro Fino",
+      mensalista: false,
+      mensal_pago: false,
+      idade: 18,
+      bebida: {
+        quente: "Agua de Coco", 
+        cerveja: "Fanta Uva"
+      }
+    }
+  ]
+ 
   return (
     <div className="App">
-      Borjao ddd
+    
     </div>
   );
 }
